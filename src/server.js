b07d8c8c1a853abd6b5b435fbab311cf4615f7f2
newExpand <magin0auto@gmail.com>
2023-02-03 00:18:35 +0900

    feat: 홈말고 다른 라우팅 사용안하기 위해 리다이렉트 설정

--- src/server.js
+++ src/server.js
@@ -8,5 +8,6 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 app.listen(3000);
