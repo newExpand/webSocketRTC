9c097b109db67c4940428daf584be8a9d02db92e
newExpand <magin0auto@gmail.com>
2023-02-03 10:45:33 +0900

    feat : websocket에 메세지 전달 및 양방향 통신 테스트

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("연결 확인!")
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("연결됐음!");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("문자왔숑! => ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("야 너 연결끊겼다ㅋㅋㅋ");
+});
+
+setTimeout(() => {
+    socket.send("나 브라우전데 너 뉘기야?");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -16,6 +16,21 @@ const handleListen = () => console.log(`http://localhost:3000`);
 
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({server})
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+    console.log("닫힘??ㅋㅋ");
+}
+
+function onSocketMessage(message) {
+    console.log(message.toString("utf-8"));
+}
+
+wss.on("connection", (socket) => {
+    console.log("갔냐?");
+    socket.on("close", onSocketClose);
+    socket.on("message", onSocketMessage);
+    socket.send("안뇽!@");
+});
 
 server.listen(3000, handleListen);
